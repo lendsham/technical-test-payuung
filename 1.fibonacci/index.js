2236b083c9a6439229f0ce86b7f25b72f54146f4
nushairfalah <lendsham1st@gmail.com>
2022-04-06 10:07:45 +0700

    fibonacci done

--- 1.fibonacci/index.js
+++ 1.fibonacci/index.js
@@ -1,7 +1,8 @@
-function fibonacciGenerator(n) {
-    let a = 0
-    let b = 1
-    let next
+function fibonacci(n) {
+    n = +n;
+    let a = 0;
+    let b = 1;
+    let next;
     let string = '';
 
     for (let i = 0; i <= n; i++) {
